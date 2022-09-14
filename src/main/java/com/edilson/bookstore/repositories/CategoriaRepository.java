e415906e3ae2fcea1dacb307dbcaa1533d3565d1
Edilson10 <edilsonnhancale1@gmail.com>
2022-09-14 19:13:59 +0200

    perfil de teste implementado

--- src/main/java/com/edilson/bookstore/repositories/CategoriaRepository.java
+++ src/main/java/com/edilson/bookstore/repositories/CategoriaRepository.java
@@ -3,9 +3,9 @@ package com.edilson.bookstore.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.edilson.bookstore.domain.Livro;
+import com.edilson.bookstore.domain.Categoria;
 
 @Repository
-public interface CategoriaRepository extends JpaRepository<Livro, Integer> {
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
 
 }
