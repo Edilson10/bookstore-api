39f88466a63fc19368f5bf729567af5bb1d16b48
Edilson10 <edilsonnhancale1@gmail.com>
2022-09-15 14:25:09 +0200

    findAll para categoria

--- src/main/java/com/edilson/bookstore/service/CategoriaService.java
+++ src/main/java/com/edilson/bookstore/service/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.edilson.bookstore.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +21,10 @@ public class CategoriaService {
 		Optional<Categoria> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto nao encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
+	
+	public List<Categoria> findAll(){
+		return repository.findAll();
 	}
 
 }
