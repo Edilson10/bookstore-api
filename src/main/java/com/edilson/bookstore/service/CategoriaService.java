2b117a9b7fd0f7eb5c95e4eb4a52f48d697b60fc
Edilson10 <edilsonnhancale1@gmail.com>
2022-09-15 15:20:15 +0200

    Delete para categoria

--- src/main/java/com/edilson/bookstore/service/CategoriaService.java
+++ src/main/java/com/edilson/bookstore/service/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.edilson.bookstore.domain.Categoria;
+import com.edilson.bookstore.dtos.CategoriaDTO;
 import com.edilson.bookstore.repositories.CategoriaRepository;
 import com.edilson.bookstore.service.exceptions.ObjectNotFoundException;
 
@@ -30,6 +31,18 @@ public class CategoriaService {
 	public Categoria create(Categoria obj) {
 		obj.setId(null);
 		return repository.save(obj);
+	}
+
+	public Categoria create(Integer id, CategoriaDTO objDto) {
+		Categoria obj = findById(id);
+		obj.setNome(objDto.getNome());
+		obj.setDescricao(objDto.getDescricao());
+		return repository.save(obj);
+	}
+
+	public void delete(Integer id) {
+		findById(id);
+		repository.deleteById(id);
 	}
 
 }
